f267d1bef91047b64aa3a411f3245175d57644d8
IsrakUAP <israk2301@gmail.com>
2023-08-20 13:45:05 +0600

    added condition for purchase amount 200

--- script/script.js
+++ script/script.js
@@ -9,14 +9,27 @@ function card(target) {
     const order = document.getElementById("order-list");
     let list = document.createElement("li");
     const ol = order.getElementsByTagName("li");
-    list.innerText = ol.length + 1 + "."+" "+ listItem;
+    list.innerText = ol.length + 1 + "." + " " + listItem;
     order.appendChild(list);
     const price = target.childNodes[3].childNodes[5].innerText.split(" ")[0];
     total = total + parseFloat(price);
     const totalPrice = document.getElementById("total-price");
     totalPrice.innerHTML = total;
-    const discountPrice =document.getElementById("discount-price");
-    const discount =total/100*20;
-    discountPrice.innerText=discount.toFixed(2);
+    const couponBtn = document.getElementById("coupon-btn");
+    if (total <= 200) {
+        couponBtn.disabled = true;
+        couponBtn.background = "black";
+
+        return;
+    }
+    else {
+        couponBtn.disabled = false;
+        couponBtn.style.background = "#E527B2";
+        couponBtn.style.cursor = "pointer"
+        const discountPrice = document.getElementById("discount-price");
+        const discount = total / 100 * 20;
+        discountPrice.innerText = discount.toFixed(2);
+        return;
+    }
 }
 
