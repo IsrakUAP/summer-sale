12cd55bc114ab4146caccba6739957751a606aae
IsrakUAP <israk2301@gmail.com>
2023-08-20 16:42:09 +0600

    purchase button added with modal home button

--- script/script.js
+++ script/script.js
@@ -16,6 +16,7 @@ function card(target) {
     const totalPrice = document.getElementById("total-price");
     totalPrice.innerHTML = total;
     const couponBtn = document.getElementById("coupon-btn");
+    const purchaseBtn = document.getElementById("purchase-btn");
 
     couponBtn.addEventListener("click", function () {
         const couponCode = document.getElementById("coupon-code").value;
@@ -24,7 +25,6 @@ function card(target) {
         if (couponCode === targetCoupon && total >=200) {
             couponBtn.style.cursor = "default";
             couponBtn.style.cursor = "pointer";
-
             const discountPrice = document.getElementById("discount-price");
             const discount = total * 0.2;
             discountPrice.innerText = discount.toFixed(2);
@@ -33,9 +33,21 @@ function card(target) {
             const totalCost = total - discount;
             totalAmount.innerText = totalCost.toFixed(2);
         }
+        
     });
+    purchaseBtn.addEventListener("click", function () {
+    if(total >0){
+        purchaseBtn.style.cursor = "default";
+        purchaseBtn.style.cursor = "pointer";
+        my_modal_1.showModal();
+        const homeBtn = document.getElementById("home-btn");
+        homeBtn.addEventListener("click",function(){
+            window.location.href="index.html";
+        })
 
-
+        return;
+    }
+});
     if (total < 200 && couponCode.value !== "SELL200") {
         return;
     }
