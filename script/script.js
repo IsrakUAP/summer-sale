ed3d84e7f65a3cb5a9722bb479208427b89787b3
IsrakUAP <israk2301@gmail.com>
2023-08-20 12:29:09 +0600

    total price added for cart

--- script/script.js
+++ script/script.js
@@ -1,15 +1,20 @@
-document.getElementById("promo-btn").addEventListener("click",function(){
+document.getElementById("promo-btn").addEventListener("click", function () {
     const coupon = document.getElementById("coupon-code");
     coupon.value = "SELL200";
 
 })
-
-function card(target){
+let total = 0;
+function card(target) {
     const listItem = target.childNodes[3].childNodes[3].innerText;
-    const order= document.getElementById("order-list"); 
+    const order = document.getElementById("order-list");
     let list = document.createElement("li");
     const ol = order.getElementsByTagName("li");
-    list.innerText=ol.length+1+"."+listItem;
+    list.innerText = ol.length + 1 + "." + listItem;
     order.appendChild(list);
+    const price = target.childNodes[3].childNodes[5].innerText.split(" ")[0];
+    total = total + parseFloat(price);
+    console.log(total);
+    const totalPrice = document.getElementById("total-price");
+    totalPrice.innerHTML = total;
 }
 
